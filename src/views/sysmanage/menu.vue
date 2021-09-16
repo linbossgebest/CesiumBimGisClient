538a88fb5eee593228f785d7f32c28a253ff4a56
linbossgebest <809621397@qq.com>
2021-09-16 17:44:45 +0800

    update menu

--- src/views/sysmanage/menu.vue
+++ src/views/sysmanage/menu.vue
@@ -57,16 +57,20 @@
 
       <el-table-column align="header-center" label="排序">
         <template slot-scope="scope">
-          {{ scope.row.order }}
+          {{ scope.row.orderno }}
         </template>
       </el-table-column>
 
       <el-table-column align="center" label="操作">
-        <template slot-scope="scope">
-          <el-button type="primary" size="small" @click="handleUpdate(scope)">
+        <template slot-scope="{ row, $index }">
+          <el-button type="primary" size="small" @click="handleUpdate(row)">
             编辑
           </el-button>
-          <el-button type="danger" size="small" @click="handleDelete(scope)">
+          <el-button
+            type="danger"
+            size="small"
+            @click="handleDelete(row, $index)"
+          >
             删除
           </el-button>
         </template>
@@ -86,10 +90,10 @@
         label-width="100px"
         style="width: auto; margin-left: 50px"
       >
-        <el-form-item label="菜单名称" prop="MenuName">
+        <el-form-item label="菜单名称" prop="name">
           <el-input v-model="temp.name" />
         </el-form-item>
-        <el-form-item label="父级菜单" prop="ParentId">
+        <el-form-item label="父级菜单">
           <el-cascader
             v-model="selectedOptions"
             :key="cascaderkey"
@@ -110,8 +114,8 @@
         <el-form-item label="重定向地址">
           <el-input v-model="temp.redirect" />
         </el-form-item>
-        <el-form-item label="排序编号" prop="OrderNo">
-          <el-input v-model="temp.order" />
+        <el-form-item label="排序编号" prop="orderno">
+          <el-input v-model="temp.orderno" />
         </el-form-item>
         <el-form-item label="根菜单显示">
           <el-switch v-model="temp.alwaysShow"></el-switch>
@@ -120,7 +124,7 @@
           <el-switch v-model="temp.hidden"></el-switch>
         </el-form-item>
         <el-form-item label="是否缓存">
-          <el-switch v-model="temp.noCache"></el-switch>
+          <el-switch v-model="temp.meta.noCache"></el-switch>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -156,9 +160,9 @@ export default {
   },
   data() {
     return {
-      cascaderkey:1,
+      cascaderkey: 1,
       menuList: [],
-      menus:[],
+      menus: [],
       selectedOptions: [],
       tableKey: 0,
       total: 0,
@@ -176,7 +180,7 @@ export default {
         hidden: false,
         component: "",
         alwaysShow: false,
-        order: undefined,
+        orderno: undefined,
         meta: {
           id: undefined,
           menuid: undefined,
@@ -196,13 +200,8 @@ export default {
         update: "修改菜单",
       },
       rules: {
-        MenuName: [
-          { required: true, message: "请输菜单名称", trigger: "blur" },
-        ],
-        ParentId: [
-          { required: true, message: "请选择父菜单信息", trigger: "blur" },
-        ],
-        OrderNo: [
+        name: [{ required: true, message: "请输菜单名称", trigger: "blur" }],
+        orderno: [
           { required: true, message: "请输入排序编号", trigger: "blur" },
         ],
       },
@@ -217,12 +216,17 @@ export default {
       getRoutes().then((response) => {
         let data = JSON.parse(response.data).menuTree;
         this.menuList = data;
-        // this.menus.concat(data);
-        let m={'id':0,'name':'顶级菜单'}
-        m.children=data;
-       this.menus.push(m);
-       this.cascaderkey++;
-        console.log( this.menus);
+        console.log(this.menuList);
+        this.menus = [];
+        let m = {
+          id: 0,
+          name: "顶级菜单",
+          children: [],
+        };
+        m.children = data;
+        this.menus.push(m);
+        this.cascaderkey++;
+        //console.log(this.menus);
       });
     },
     handleFilter() {
@@ -240,7 +244,7 @@ export default {
         hidden: false,
         component: "",
         alwaysShow: false,
-        order: undefined,
+        orderno: undefined,
         meta: {
           id: undefined,
           menuid: undefined,
@@ -256,6 +260,7 @@ export default {
     },
     handleCreate() {
       this.resetTemp();
+      this.selectedOptions = [0]; //创建时默认父级菜单选择顶级菜单
       this.dialogStatus = "create";
       this.dialogFormVisible = true;
       this.$nextTick(() => {
@@ -265,23 +270,34 @@ export default {
     createData() {
       this.$refs["dataForm"].validate((valid) => {
         if (valid) {
-          addMenu(this.temp).then(() => {
+          this.temp.parentid =
+            this.selectedOptions[this.selectedOptions.length - 1];
+          console.log(this.temp);
+          addMenu(this.temp).then((response) => {
+            //console.log(response);
             this.dialogFormVisible = false;
-            this.$notify({
-              title: "成功",
-              message: "创建成功",
-              type: "success",
-              duration: 2000,
-            });
+            if (response.isSuccess) {//判断返回值response是否成功
+              this.$notify({
+                title: "成功",
+                message: "创建成功",
+                type: "success",
+                duration: 2000,
+              });
+            }
             this.getRoutes();
           });
         }
       });
     },
     handleUpdate(row) {
-      this.temp = Object.assign({}, row.row); // copy obj
-      this.selectedOptions=[this.temp.parentid];
-      console.log(this.selectedOptions);
+      this.temp = Object.assign({}, row); // copy obj
+      if (this.temp.parentid == 0) {
+        //父级菜单为0则是顶级菜单
+        this.selectedOptions = [this.temp.parentid];
+      } else {
+        //不是顶级菜单需要加上0 识别路径
+        this.selectedOptions = [0, this.temp.parentid];
+      }
       this.dialogStatus = "update";
       this.dialogFormVisible = true;
       this.$nextTick(() => {
@@ -292,15 +308,21 @@ export default {
       this.$refs["dataForm"].validate((valid) => {
         if (valid) {
           const tempData = Object.assign({}, this.temp);
+          tempData.parentid =
+            this.selectedOptions[this.selectedOptions.length - 1];
+          // console.log(this.selectedOptions);
+          console.log(tempData);
           // tempData.RoleIds = [];
-          addMenu(tempData).then(() => {
+          addMenu(tempData).then((response) => {
             this.dialogFormVisible = false;
-            this.$notify({
-              title: "成功",
-              message: "更新成功",
-              type: "success",
-              duration: 2000,
-            });
+            if (response.isSuccess) {
+              this.$notify({
+                title: "成功",
+                message: "更新成功",
+                type: "success",
+                duration: 2000,
+              });
+            }
             this.getRoutes();
           });
         }
@@ -308,13 +330,15 @@ export default {
     },
     handleDelete(row, index) {
       this.$confirm("请确认是否确认删除？").then(() => {
-        deleteMenu(row.Id).then(() => {
-          this.$notify({
-            title: "成功",
-            message: "删除成功",
-            type: "success",
-            duration: 2000,
-          });
+        deleteMenu(row.id).then((response) => {
+          if (response.isSuccess) {
+            this.$notify({
+              title: "成功",
+              message: "删除成功",
+              type: "success",
+              duration: 2000,
+            });
+          }
           this.getRoutes();
         });
       });
